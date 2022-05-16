f223e639feea6f306fe98b8b527b3e7ddea8201e
Boulder77 <islasmd@gmail.com>
2022-05-16 16:48:20 -0700

    CustomerDetailsPage/CustomerViewModel updates

---
+++ EZDMS.App/Pages/CustomerDetailsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EZDMS.App
+
+{
+    /// <summary>
+    /// Interaction logic for BuyerDetailsPage.xaml
+    /// </summary>
+    public partial class CustomerDetailsPage : BasePage<CustomerViewModel>
+
+    {
+
+        public CustomerDetailsPage() : base()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with specific view model
+        /// </summary>
+        /// <param name="specificViewModel">The specific view model to use for this page</param>
+        public CustomerDetailsPage(CustomerViewModel specificViewModel) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
---
+++ EZDMS.App/ViewModel/Customer/CustomerViewModel.cs
@@ -0,0 +1,126 @@
+﻿using Dna;
+using EZDMS.App.Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static EZDMS.App.DI;
+
+namespace EZDMS.App
+{
+    /// <summary>
+    /// The view model for a sales deal card control
+    /// </summary>
+    public class CustomerViewModel : BaseViewModel
+    {
+
+        #region Public Properties
+       
+        /// <summary>
+        /// The customer full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The customer number
+        /// </summary>
+        public TextEntryVertControl CustomerNumber { get; set; }
+
+        /// <summary>
+        /// The customer first name
+        /// </summary>
+        public TextEntryVertControl FirstName { get; set; }
+
+        /// <summary>
+        /// The customer middle name
+        /// </summary>
+        public TextEntryVertControl MiddleName { get; set; }
+
+        /// <summary>
+        /// The customer last name
+        /// </summary>
+        public TextEntryVertControl LastName { get; set; }
+
+        /// <summary>
+        /// The customer street address
+        /// </summary>
+        public TextEntryVertControl StreetAddress { get; set; }
+
+        /// <summary>
+        /// The customer city
+        /// </summary>
+        public TextEntryVertControl City { get; set; }
+
+        /// <summary>
+        /// The customer county
+        /// </summary>
+        public TextEntryVertControl County { get; set; }
+
+        /// <summary>
+        /// The customer state
+        /// </summary>
+        public TextEntryVertControl State { get; set; }
+
+        /// <summary>
+        /// The customer zip code
+        public TextEntryVertControl Zip { get; set; }
+
+        /// <summary>
+        /// The customer home phone
+        /// </summary>
+        public TextEntryVertControl HomePhone { get; set; }
+
+        /// <summary>
+        /// The customer work phone
+        /// </summary>
+        public TextEntryVertControl WorkPhone { get; set; }
+
+        /// <summary>
+        /// The customer cell phone
+        /// </summary>
+        public TextEntryVertControl CellPhone { get; set; }
+
+        /// <summary>
+        /// The customer date of birth
+        /// </summary>
+        public DateSelectionControl DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The customer created date
+        /// </summary>
+        public DateSelectionControl CreateDate { get; set; }
+
+        /// <summary>
+        /// The customer last activity date
+        /// </summary>
+        public DateSelectionControl LastActivityDate { get; set; }
+
+
+
+
+
+
+
+        #endregion
+
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CustomerViewModel()
+        {
+           
+
+        }
+
+        #endregion
+
+
+
+
+    }
+
+}
