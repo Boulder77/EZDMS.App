f6ffe85a0ac5a640567696ce720a67c6a692180d
Boulder77 <islasmd@gmail.com>
2022-05-28 12:36:53 -0700

    vehicle basic info VM

---
+++ EZDMS.App/ViewModel/Vehicle/Design/VehicleBasicInfoDesignModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System;
+using EZDMS.App.Core;
+
+namespace EZDMS.App
+{
+    /// <summary>
+    /// The design-time data for a <see cref="VehicleBasicInfoDesignModel"/>
+    /// </summary>
+    public class VehicleBasicInfoDesignModel : VehicleBasicInfoViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static VehicleBasicInfoDesignModel Instance => new VehicleBasicInfoDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public VehicleBasicInfoDesignModel()
+
+        {
+            
+            StockNumber = new TextEntryViewModel { Label = "Stock Number", OriginalText = "P1807" };
+            VIN = new TextEntryViewModel { Label = "VIN", OriginalText = "1GNSKBKC2JR177272" };
+            Type = new TextEntryViewModel { Label = "Type", OriginalText = "Used" };
+            Status = new TextEntryViewModel { Label = "Status", OriginalText = "In Stock" };
+            Year = new TextEntryViewModel { Label = "Year", OriginalText = "2018" };            
+            Make = new TextEntryViewModel { Label = "Make", OriginalText = "Chevrolet" };
+            Model = new TextEntryViewModel { Label = "Model", OriginalText = "Tahoe" };
+            Trim = new TextEntryViewModel { Label = "Trim", OriginalText = "LT" };
+            Body = new TextEntryViewModel { Label = "Body", OriginalText = "Utility" };
+            ExteriorColor = new TextEntryViewModel { Label = "Exterior Color", OriginalText = "Silver" };
+            InteriorColor = new TextEntryViewModel { Label = "Interior Color", OriginalText = "Black" };
+            Class = new TextEntryViewModel { Label = "Class", OriginalText = "Passenger" };
+            Odometer = new TextEntryViewModel { Label = "Odometer", OriginalText = "34,555" };
+            OdometerStatus = new TextEntryViewModel { Label = "Odometer Status", OriginalText = "Actual Miles" };
+
+        }
+
+        #endregion
+    }
+}
---
+++ EZDMS.App/ViewModel/Vehicle/VehicleBasicInfoViewModel.cs
@@ -0,0 +1,103 @@
+﻿namespace EZDMS.App
+{
+    /// <summary>
+    /// The view model for the vehicle basic info control
+    /// <summary>
+    public class VehicleBasicInfoViewModel : BaseViewModel
+    {
+
+        #region Public Properties
+
+        /// <summary>
+        /// The description of the vehicle
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The VIN of the vehicle
+        /// </summary>
+        public TextEntryViewModel VIN { get; set; }
+
+        /// <summary>
+        /// The stock number of the vehicle
+        /// </summary>
+        public TextEntryViewModel StockNumber { get; set; }
+
+        /// <summary>
+        /// The type of the vehicle
+        /// </summary>
+        public TextEntryViewModel Type { get; set; }
+
+        /// <summary>
+        /// The status of the vehicle
+        /// </summary>
+        public TextEntryViewModel Status { get; set; }
+
+        /// <summary>
+        /// The year of the vehicle
+        /// </summary>
+        public TextEntryViewModel Year { get; set; }
+
+        /// <summary>
+        /// The make of the vehicle
+        /// </summary>
+        public TextEntryViewModel Make { get; set; }
+
+        /// <summary>
+        /// The model of the vehicle
+        /// </summary>
+        public TextEntryViewModel Model { get; set; }
+
+        /// <summary>
+        /// The body of the vehicle
+        /// </summary>
+        public TextEntryViewModel Body { get; set; }
+
+        /// <summary>
+        /// The trim of the vehicle
+        /// </summary>
+        public TextEntryViewModel Trim { get; set; }
+
+        /// <summary>
+        /// The exterior color of the vehicle
+        /// </summary>
+        public TextEntryViewModel ExteriorColor { get; set; }
+
+        /// <summary>
+        /// The interior color of the vehicle
+        /// </summary>
+        public TextEntryViewModel InteriorColor { get; set; }
+
+        /// <summary>
+        /// The class of the vehicle
+        /// </summary>
+        public TextEntryViewModel Class { get; set; }
+
+        /// <summary>
+        /// The odometer of the vehicle
+        /// </summary>
+        public TextEntryViewModel Odometer { get; set; }
+
+        /// <summary>
+        /// The odometer status of the vehicle
+        /// </summary>
+        public TextEntryViewModel OdometerStatus { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public VehicleBasicInfoViewModel()
+        {
+
+
+
+
+        }
+
+        #endregion
+    }
+}
