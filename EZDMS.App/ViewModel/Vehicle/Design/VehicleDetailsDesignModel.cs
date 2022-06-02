669478f8861d4c90bbd0e41f1327a6ccd156036d
Boulder77 <islasmd@gmail.com>
2022-06-02 10:20:45 -0700

    vehicle sales control updates

--- EZDMS.App/ViewModel/Vehicle/Design/VehicleDetailsDesignModel.cs
+++ EZDMS.App/ViewModel/Vehicle/Design/VehicleDetailsDesignModel.cs
@@ -16,37 +16,37 @@ namespace EZDMS.App
         /// A single instance of the design model
         /// <summary>
         public static VehicleDetailsDesignModel Instance => new VehicleDetailsDesignModel();
-    
-#endregion
+
+        #endregion
 
         #region Constructor
 
         /// <summary>
         /// Default constructor
         /// <summary>
-public VehicleDetailsDesignModel()
+        public VehicleDetailsDesignModel()
         {
 
-            NumberOfDoors = new TextEntryViewModel { Label = "NumberOfDoors", OriginalText = "myNumberOfDoors" };
+            NumberOfDoors = new TextEntryViewModel { Label = "Number Of Doors", OriginalText = "myNumberOfDoors" };
             Cylinders = new TextEntryViewModel { Label = "Cylinders", OriginalText = "myCylinders" };
-            FuelType = new TextEntryViewModel { Label = "FuelType", OriginalText = "myFuelType" };
-            FuelSystem = new TextEntryViewModel { Label = "FuelSystem", OriginalText = "myFuelSystem" };
-            FuelEconomy = new TextEntryViewModel { Label = "FuelEconomy", OriginalText = "myFuelEconomy" };
-            TransmissionType = new TextEntryViewModel { Label = "TransmissionType", OriginalText = "myTransmissionType" };
-            TransmissionSpeed = new TextEntryViewModel { Label = "TransmissionSpeed", OriginalText = "myTransmissionSpeed" };
+            FuelType = new TextEntryViewModel { Label = "Fuel Type", OriginalText = "myFuelType" };
+            FuelSystem = new TextEntryViewModel { Label = "Fuel System", OriginalText = "myFuelSystem" };
+            FuelEconomy = new TextEntryViewModel { Label = "Fuel Economy", OriginalText = "myFuelEconomy" };
+            TransmissionType = new TextEntryViewModel { Label = "Transmission Type", OriginalText = "myTransmissionType" };
+            TransmissionSpeed = new TextEntryViewModel { Label = "Transmission Speed", OriginalText = "myTransmissionSpeed" };
             Drivetrain = new TextEntryViewModel { Label = "Drivetrain", OriginalText = "myDrivetrain" };
             Engine = new TextEntryViewModel { Label = "Engine", OriginalText = "myEngine" };
-            EngineType = new TextEntryViewModel { Label = "EngineType", OriginalText = "myEngineType" };
-            EngineSerialNumber = new TextEntryViewModel { Label = "EngineSerialNumber", OriginalText = "myEngineSerialNumber" };
-            IgnitionKeyCode = new TextEntryViewModel { Label = "IgnitionKeyCode", OriginalText = "myIgnitionKeyCode" };
-            TrunkKeyCode = new TextEntryViewModel { Label = "TrunkKeyCode", OriginalText = "myTrunkKeyCode" };
+            EngineType = new TextEntryViewModel { Label = "Engine Type", OriginalText = "myEngineType" };
+            EngineSerialNumber = new TextEntryViewModel { Label = "Engine Serial Number", OriginalText = "myEngineSerialNumber" };
+            IgnitionKeyCode = new TextEntryViewModel { Label = "Ignition Key Code", OriginalText = "myIgnitionKeyCode" };
+            TrunkKeyCode = new TextEntryViewModel { Label = "Trunk Key Code", OriginalText = "myTrunkKeyCode" };
             Weight = new TextEntryViewModel { Label = "Weight", OriginalText = "myWeight" };
-            LicensePlate = new TextEntryViewModel { Label = "LicensePlate", OriginalText = "myLicensePlate" };
-            LicenseState = new TextEntryViewModel { Label = "LicenseState", OriginalText = "myLicenseState" };
-            LicenseExpirationDate = new DateSelectionViewModel { Label = "LicenseExpirationDate", Date = Convert.ToDateTime("02/05/1991") };
-            LotLocation = new TextEntryViewModel { Label = "LotLocation", OriginalText = "myLotLocation" };
+            LicensePlate = new TextEntryViewModel { Label = "License Plate", OriginalText = "myLicensePlate" };
+            LicenseState = new TextEntryViewModel { Label = "License State", OriginalText = "myLicenseState" };
+            LicenseExpirationDate = new DateSelectionViewModel { Label = "License Expiration Date", Date = Convert.ToDateTime("02/05/1991") };
+            LotLocation = new TextEntryViewModel { Label = "Lot Location", OriginalText = "myLotLocation" };
             Style = new TextEntryViewModel { Label = "Style", OriginalText = "myStyle" };
-            ModelCode = new TextEntryViewModel { Label = "ModelCode", OriginalText = "myModelCode" };
+            ModelCode = new TextEntryViewModel { Label = "Model Code", OriginalText = "myModelCode" };
         }
 
         #endregion
