1a06cd6a02b73dbfc1f01a40646ed196502d7334
Matt Islas <islasm@proquotes.net>
2024-06-20 23:31:15 -0700

    Dialog updates

---
+++ EZDMS.App/Dialogs/DialogTotalFeesSales.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EZDMS.App
+{
+    /// <summary>
+    /// Interaction logic for DialogTotalFeesSales.xaml
+    /// </summary>
+    public partial class DialogTotalFeesSales : BaseDialogUserControl
+    {
+        public DialogTotalFeesSales()
+        {
+            InitializeComponent();                       
+        }
+    }
+}
